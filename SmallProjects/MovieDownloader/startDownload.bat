a12546bad02de8c7353a77eebace8f1f54118e8e
dan71853 <dan71853@gmail.com>
2024-01-10 17:03:21 +1300

    Increased delay, nordvpn is slower to open

--- SmallProjects/MovieDownloader/startDownload.bat
+++ SmallProjects/MovieDownloader/startDownload.bat
@@ -1,6 +1,6 @@
 @echo off
 start "" "C:\Program Files\NordVPN\NordVPN.exe"
-timeout /t 15 >nul
+timeout /t 18 >nul
 start "" "C:\Program Files\qBittorrent\qbittorrent.exe" 
 timeout /t 5 >nul
 start "" python "%~dp0downloadNyaa.py"
