d123df829f8b1987b8998107383c71ca85bc403a
dan71853 <dan71853@gmail.com>
2026-07-12 15:29:21 +1200

    Fixed torrent save path

--- SmallProjects/MovieDownloader/config.py
+++ SmallProjects/MovieDownloader/config.py
@@ -1,6 +1,6 @@
 # DO NOT USE "[ToonsHub]"
 search_keywords = ["[EMBER]", "[Xtrem]","[DKB]","[SubsPlease]","[LostYears]","[Judas]","[Yameii]", "[Sokudo]"]
 
-base_save_path = "/run/media/dan/OldHDD/Downloads/Anime/"
+base_save_path = "/mnt/OldHDD/Downloads/Anime/"
 
 json_dir = './shows.json'
